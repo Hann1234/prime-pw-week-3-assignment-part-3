2aac6510c2542a4dffddba28e67719faaef94be9
Hann1234 <christopherjhannigan@gmail.com>
2021-05-23 10:19:22 -0500

    done with q8

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -51,13 +51,22 @@ for (supplies of supplyChanges) {
     console.log(`Added ${supplies} parts.`);
   }
   else if (supplies < 0) {
-    console.log(`Part count ${supplies}.`)
+    console.log(`Part count ${supplies}.`);
   }
 }
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
-
+let x = 0;
+while ( x < supplyChanges.length){
+  if (supplyChanges[x] > 0) {
+    console.log(`Added ${supplyChanges[x]} parts.`);
+  }
+  else if (supplyChanges[x] < 0) {
+    console.log(`Part count ${supplyChanges[x]}.`);
+  }
+  x++;
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
